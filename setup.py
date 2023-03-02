507d04d43a81602959e29eecfea51404b791a6d4
guillemdb <guillem@fragile.tech>
2023-03-02 09:23:52 +0100

    Fix random bug in setup
    Signed-off-by: guillemdb <guillem@fragile.tech>

--- setup.py
+++ setup.py
@@ -10,31 +10,10 @@ from version import VERSION
 
 # Environment-specific dependencies.
 extras = {
-    "atari": ["ale-py~=0.7.1"],
-    "accept-rom-license": ["autorom[accept-rom-license]~=0.4.2"],
+    "atari": ["ale-py==0.7.1"],
+    "accept-rom-license": ["autorom[accept-rom-license]==0.4.2"],
     "box2d": ["box2d-py==2.3.5", "pyglet>=1.4.0"],
-    "classic_control": ["pyglet>=1.4.0"],
-    "mujoco": ["mujoco_py>=1.50, <2.0"],
-    "robotics": ["mujoco_py>=1.50, <2.0"],
-    "toy_text": ["scipy>=1.4.1"],
-    "other": ["lz4>=3.1.0", "opencv-python>=3."],
 }
-
-# Meta dependency groups.
-nomujoco_blacklist = set(["mujoco", "robotics", "accept-rom-license"])
-nomujoco_groups = set(extras.keys()) - nomujoco_blacklist
-
-extras["nomujoco"] = list(
-    itertools.chain.from_iterable(map(lambda group: extras[group], nomujoco_groups))
-)
-
-
-all_blacklist = set(["accept-rom-license"])
-all_groups = set(extras.keys()) - all_blacklist
-
-extras["all"] = list(
-    itertools.chain.from_iterable(map(lambda group: extras[group], all_groups))
-)
 
 setup(
     name="gym",
@@ -49,7 +28,7 @@ setup(
     install_requires=[
         "numpy>=1.18.0",
         "cloudpickle>=1.2.0",
-        "importlib_metadata>=4.8.1; python_version < '3.8'",
+        "importlib_metadata>=4.8.1; python_version <= '3.8'",
     ],
     extras_require=extras,
     package_data={
